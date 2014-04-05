189be77f1c195470da5e40657b522655c6b0be30
luizventurote <luiz_17@ymail.com>
2014-04-05 15:09:25 -0300

    Template 404 added.

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * The template for displaying 404 pages (Not Found).
+ *
+ * @package WordPress
+ * @subpackage start-wp
+ * @since Start WP Framework v0.0.1
+ */
+
+get_header(); ?>
+
+	<h1>Error 404 - Not Found</h1>
+	
+<?php get_footer(); ?>
