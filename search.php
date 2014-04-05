92dc2d9ef520e74c9d4d1609f3ac93db4bbd695c
luizventurote <luiz_17@ymail.com>
2014-04-05 15:14:37 -0300

    Template for displaying Search Results added.

---
+++ search.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * The template for displaying Search Results pages.
+ *
+ * @package WordPress
+ * @subpackage start-wp
+ * @since Start WP Framework v0.0.1
+ */
+
+get_header(); ?>
+
+	<?php
+		// Start the Loop.
+		while ( have_posts() ) : the_post();
+
+			// The page content.
+			the_content();
+
+		endwhile;
+	?>
+	
+<?php get_footer(); ?>
