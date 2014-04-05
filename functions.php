9126470c72588489a5d6ea2d506739e3a5c88d34
luizventurote <luiz_17@ymail.com>
2014-04-05 15:12:23 -0300

    Functions file added.

--- functions.php
+++ functions.php
@@ -1,6 +1,14 @@
 <?php
 
 	/**
+	 * Start WP Framework functions and definitions.
+	 *
+	 * @package WordPress
+	 * @subpackage start-wp
+	 * @since Start WP Framework v0.0.1
+	 */
+
+	/**
 	 * Boot theme
 	 */
 	require_once ('core/init.php');
